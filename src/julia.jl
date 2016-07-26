f4ee3315526a8d13c20ba73f05650d1d2b33bdd4
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2016-07-26 04:23:16 +0200

    Improve multireshape documentation

--- src/julia.jl
+++ src/julia.jl
@@ -1,31 +1,16 @@
 module Julia
 
-if VERSION >= v"0.5-"
-    @doc """
+"""
 `multireshape(A, dims1, dims2[, ...])`
 
-Create a vector of reshaped views of `A`, referencing the same data.
+Create a vector of reshaped views of `A` with given dimensions.
+The reshaped views reference the original data, there is no copying.
+In Julia versions prior to 0.5, the data _is_ copied.
 
-    julia> xs = zeros(126);
+    julia> xs = rand(126);
     julia> Xs = multireshape(xs, (2,3), (4,5,6));
     julia> # `Xs` is a vector of multidimensional arrays (one 2×3 matrix and 4×5×6 tensor).
-    julia> # Now, the elements of xs and Xs reference the same memory, no copying.
-    julia> xs[5] = 1;
-    julia> Xs[1][1,3]
-    1
-    julia> Xs[2][4,5,6] = 100;
-    julia> xs[126]
-    2
-""" -> function multireshape end
-else
-    @doc """
-`multireshape(A, dims1, dims2[, ...])`
-
-Create a vector of reshaped views of `A`, copying the original data.
-Use Julia 0.5 to avoid copying, i.e. the result of multireshape references the same data.
-""" -> function multireshape end
-end
-
+"""
 function multireshape(parent::AbstractArray, dimss::Tuple{Vararg{Int}}...)
     n = length(parent)
     sum(map(prod, dimss)) == n || throw(DimensionMismatch("parent has $n elements, which is incompatible with size $dimss"))
