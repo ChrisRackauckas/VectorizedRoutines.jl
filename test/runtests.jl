97b1035a470ae145e521461d317b3294deabbe06
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2016-07-26 19:22:02 -0700

    Add some MATLAB tests, organize a bit.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,27 @@
 using VectorizedRoutines
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# MATLAB
+
+bool1 = size(Matlab.meshgrid(0:0.1:1),1) == 2
+bool2 = size(Matlab.meshgrid(0:0.1:1)[1]) == (11,11)
+bool3 = Matlab.meshgrid(0:0.1:1) ==Matlab.meshgrid(0:0.1:1,0:0.1:1)
+bool4 = size(Matlab.meshgrid(0:0.1:1,0:0.1:1,0:0.1:1)[1]) == (11,11,11)
+
+# R
 
 v1=1:5
 v2 = 5:-1:1
 @test R.rep(v1,v2) == [1;1;1;1;1;2;2;2;2;3;3;3;4;4;5]
+
+# Python
+
+@test (Python.@vcomp Int[i^2 for i in 1:10] when i % 2 == 0) == Int[4, 16, 36, 64, 100]
+words = ["ant", "akita", "bear", "cow"]
+@test (Python.@vcomp [uppercase(word) for word in words] when startswith(word, "a")) == Any["ANT", "AKITA"]
+@test (Python.@vcomp Tuple[(i, e) for (i, e) in enumerate(reverse(1:10))] when i-e < 0) == Tuple[(1,10),(2,9),(3,8),(4,7),(5,6)]
+
+# Julia
 
 xs = collect(1:26)
 Xs = Julia.multireshape(xs, (2,3), (4,5)) # Array of two matrices, 2×3 and 4×5
@@ -15,8 +30,3 @@ xs[26] = 42
 @test Xs[2][4,5] == 42
 Xs[2][3,5] = 100
 @test xs[25] == 100 # 100
-
-@test (Python.@vcomp Int[i^2 for i in 1:10] when i % 2 == 0) == Int[4, 16, 36, 64, 100]
-words = ["ant", "akita", "bear", "cow"]
-@test (Python.@vcomp [uppercase(word) for word in words] when startswith(word, "a")) == Any["ANT", "AKITA"]
-@test (Python.@vcomp Tuple[(i, e) for (i, e) in enumerate(reverse(1:10))] when i-e < 0) == Tuple[(1,10),(2,9),(3,8),(4,7),(5,6)]
